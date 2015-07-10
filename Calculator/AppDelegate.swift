77a0a3f0dc26c960e00bff46f66dfddb88995d5a
Артем <artem@MacBook-Pro-Artem.local>
2015-07-10 13:44:57 +0300

    lfkhgcj

--- Calculator/AppDelegate.swift
+++ Calculator/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    //kjhfkfkugfu
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
